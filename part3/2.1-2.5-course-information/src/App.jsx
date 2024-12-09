fde45767d25efc5c3fc1b0da1be74ccda06523a3
Paul Standing <paultobiasstanding@gmail.com>
2024-12-09 14:16:33 +0200

    2.1 course information using Course component and map for the course parts.

--- part3/2.1-2.5-course-information/src/App.jsx
+++ part3/2.1-2.5-course-information/src/App.jsx
@@ -1,18 +1,29 @@
-import Note from "./components/note";
+import Course from "./components/course";
 
-const App = (props) => {
-  const { notes } = props;
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
