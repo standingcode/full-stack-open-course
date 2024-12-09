a640be1f470be3bab2de304d350b785c7337ad50
Paul Standing <paultobiasstanding@gmail.com>
2024-12-09 23:41:00 +0200

    Working on filter but it's broken

--- part3/2.6-2.10-phonebook/src/App.jsx
+++ part3/2.6-2.10-phonebook/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
 
+const FilteredResults = ({ persons }) => {
+  const filteredPersons = persons.filter((person) => person.name.find("t"));
+
+  return filteredPersons.map((person) => (
+    <div key={person.name}>
+      {person.name} {person.number}
+    </div>
+  ));
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "0401234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const addNameInputFieldChanged = (event) => {
     setNewName(event.target.value);
@@ -33,9 +48,18 @@ const App = () => {
     setNewNumber("");
   };
 
+  const filterFiledChanged = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter results
+        <input value={filter} onChange={filterFiledChanged} />
+      </div>
+      <h2>Add new</h2>
       <form onSubmit={addNameInputFieldSubmitted}>
         <div>
           name: <input value={newName} onChange={addNameInputFieldChanged} />
@@ -49,11 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      <FilteredResults persons={persons} />
     </div>
   );
 };
