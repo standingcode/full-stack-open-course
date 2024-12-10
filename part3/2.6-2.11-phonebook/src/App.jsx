5afb0450496da39b8534e66ce89506ccc3b055cb
Paul Standing <paultobiasstanding@gmail.com>
2024-12-10 15:22:46 +0200

    Initial phonebook state comes from the db.json file via the server

--- part3/2.6-2.11-phonebook/src/App.jsx
+++ part3/2.6-2.11-phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import FilteredResults from "./components/FilteredResults";
 import AddUserForm from "./components/AddUserForm";
 import FilterSearchBox from "./components/FilterSearchBox.jsx";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -45,6 +41,16 @@ const App = () => {
   const filterFieldChanged = (event) => {
     setFilter(event.target.value);
   };
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   return (
     <div>
