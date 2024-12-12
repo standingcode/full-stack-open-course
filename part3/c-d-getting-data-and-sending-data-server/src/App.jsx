b1eaccbeb37917b41c7c9f62d4f6893e5f2e5faf
Paul Standing <paultobiasstanding@gmail.com>
2024-12-12 10:51:09 +0200

    Finishing up learning material on part e

--- part3/c-d-getting-data-and-sending-data-server/src/App.jsx
+++ part3/c-d-getting-data-and-sending-data-server/src/App.jsx
@@ -1,11 +1,29 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
-import noteService from "./services/notes";
+import noteService from "./services/Notes";
+import Notification from "./components/Notification";
+
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2024
+      </em>
+    </div>
+  );
+};
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
@@ -17,9 +35,13 @@ const App = () => {
         setNotes(notes.map((note) => (note.id === id ? updatedNote : note)));
       })
       .catch((error) => {
-        alert(
-          `the note '${note.content}' was already deleted from server (${error})`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -53,6 +75,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -71,6 +94,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
