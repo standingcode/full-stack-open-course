3902524e7375375db10eba02bf7ef71ea4202335
Paul Standing <paultobiasstanding@gmail.com>
2024-12-13 10:46:39 +0200

    Import weather API key from file

--- part3/2.18-2.20-data-for-countries/src/services/Weather.jsx
+++ part3/2.18-2.20-data-for-countries/src/services/Weather.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-const weatherAPIKey = import.meta.env.VITE_SOME_KEY;
+const WEATHER_API = import.meta.env.VITE_WEATHER_API;
+
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
 
 const GetWeatherForLatLong = (lat, long) => {
   return axios
-    .get(`${baseUrl}lat=${lat}&lon=${long}&units=metric&appid=${weatherAPIKey}`)
+    .get(`${baseUrl}lat=${lat}&lon=${long}&units=metric&appid=${WEATHER_API}`)
     .then((response) => response.data);
 };
 
